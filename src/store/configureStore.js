14fa36cd70383a190bb68ad7a173afb99c6cddc5
Alberto Araújo <albertoafaj@hotmail.com>
2023-02-21 16:52:42 -0300

    feat: add configure store

---
+++ src/store/configureStore.js
@@ -0,0 +1,7 @@
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
+const middleware = [...getDefaultMiddleware()];
+
+const reducer = combineReducers({})
+const store = configureStore({ reducer, middleware });
+export default store;
