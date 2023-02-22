df23499c4f7837c50e4082a45e5d6e7a4379cf7f
Alberto Araújo <albertoafaj@hotmail.com>
2023-02-22 07:57:31 -0300

    feat: create loading component

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Login from './Login'
+import Photos from './Photos'
+import { useSelector } from 'react-redux'
+import Loading from './helper/Loading'
 
 const Content = () => {
-  return (
-    <>
-      <Login />
-    </>
-  )
+
+  const { user, token } = useSelector(state => state.login)
+  if (user.loading || token.loading) return <Loading />;
+  if (user.data) return <Photos />
+  else return <Login />
 }
 
 export default Content
---
+++ src/Components/helper/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Loading.module.css';
+
+const Loading = () => {
+  return (
+    <div className={styles.loading}></div>
+  )
+}
+
+export default Loading
